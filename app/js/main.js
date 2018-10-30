5b47336209f8a56412fab28f38aca5e22c24442f
Sanzhar <sanch941@gmail.com>
2018-10-30 16:20:58 +0600

    animation global changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 //=include ../../node_modules/jquery/dist/jquery.js
 //=include ../../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js
 //=include ../../node_modules/slick-carousel/slick/slick.js
+//=include ../../node_modules/animejs/anime.min.js
 
 
 jQuery(document).ready(function($) {
@@ -185,4 +186,242 @@ jQuery(document).ready(function($) {
   window.onload = () => {
 
   };
+
+
+  // sanzhar changes
+  (function stepLeft() {
+    let stepBool = true;
+    let stepBool2 = true;
+    // STEP LEFT
+    
+    let stepLMain = document.querySelector('.--left');
+    let stepLFirst = document.querySelector('.steps-item__left-1');
+    let stepLSecond = document.querySelector('.steps-item__left-2');
+    //  STEP RIGHT
+    let stepRMain = document.querySelector('.--right');
+    let stepRFirst = document.querySelector('.steps-item__right-1');
+    let stepRSecond = document.querySelector('.steps-item__right-2');
+    // event listeners
+    stepLMain.addEventListener('click', () => {
+    // change width
+    stepLMain.querySelector('.overlay').style.opacity = '1';
+    stepRMain.querySelector('.overlay').style.opacity = '0';
+    stepLMain.classList.remove('change-width-20');
+     stepLMain.classList.add('change-width-80');
+     stepRMain.classList.add('change-width-80');
+     stepRMain.classList.add('change-width-20');
+    // change left steps    
+    stepLFirst.classList.add('beabsolute');
+    stepLSecond.classList.remove('beabsolute');
+    // change right steps 
+    stepRFirst.classList.remove('beabsolute');
+    stepRSecond.classList.add('beabsolute');
+    if(stepBool) {
+    (function animateSvg() {
+
+      var basicTimeline = anime.timeline();
+      var basicTimeline2 = anime.timeline();
+      var basicTimeline3 = anime.timeline();
+      basicTimeline
+      .add({
+        targets: ['#svg-left-1', '#svg-left-2','#svg-left-3'],
+        opacity: 1,       
+        scale: [         
+          {value: 1.1, elasticity: 100},
+          {value: 1, elasticity: 100}
+        ],
+        delay: function(el,i) {
+          return 1000 * (1 + i);
+        },
+        easing: 'easeInOutQuad',
+        offset: '-=600'
+      })
+      .add({
+        targets: ['#svg-left-4', '#svg-left-5','#svg-left-6','#svg-left-7'],
+        opacity: 1,       
+        scale: [         
+          {value: 1.1, elasticity: 100},
+          {value: 1, elasticity: 100}
+        ],
+        delay: function(el,i) {
+          return 1000 * (1 + i);
+        },
+        easing: 'easeInOutQuad',
+        offset: '-=600'
+      })
+     
+      basicTimeline2
+      .add({
+        targets: ['#p-left-1','#p-left-2','#p-left-3'],
+        opacity: 1,       
+        delay: function(el,i) {
+          return 1000 * (1 + i);
+        },
+        easing: 'easeInOutQuad',
+        offset: '-=600'
+      })
+      .add({
+        targets: ['#p-left-4','#p-left-5','#p-left-6','#p-left-7'],
+        opacity: 1,       
+        delay: function(el,i) {
+          return 1000 * (1 + i);
+        },
+        easing: 'easeInOutQuad',
+        offset: '-=600'
+      })
+
+      basicTimeline3
+      .add({
+        targets: ['#vg-left-1','#vg-left-2'],
+        opacity: 1,
+        translateX: '0',
+        width: {
+          value: '200'
+        },
+        easing: 'easeInOutQuad',
+        delay: function(el,i,l) {
+          return (1300 * (1 + i));
+        },
+        offset: '-=600'
+      })
+      .add({
+        targets: ['#vg-left-3','#vg-left-4','#vg-left-5','#vg-left-6'],
+        opacity: 1,
+        translateX: '0',
+        width: {
+          value: '200'
+        },
+        easing: 'easeInOutQuad',
+        delay: function(el,i,l) {
+          return (1100 * (1 + i));
+        },
+        offset: '-=600'
+      })
+    
+      var stepLH2 = anime({
+        targets: '.steps-item__left-2__animation h2',
+        opacity: 1,       
+        delay: 200,
+        easing: 'easeInOutQuad',
+      });
+    
+ 
+      
+    })(); stepBool = false;  }
+  
+  
+    });  
+
+    stepRMain.addEventListener('click', () => {
+      // change width
+      stepRMain.querySelector('.overlay').style.opacity = '1';
+      stepLMain.querySelector('.overlay').style.opacity = '0';
+      stepRMain.classList.remove('change-width-20');
+      stepRMain.classList.add('change-width-80');
+      stepLMain.classList.remove('change-width-80');
+      stepLMain.classList.add('change-width-20');
+     // change right steps    
+     stepRFirst.classList.add('beabsolute');
+     stepRSecond.classList.remove('beabsolute');    
+     // change left steps  
+     stepLFirst.classList.remove('beabsolute');
+    stepLSecond.classList.add ('beabsolute');
+    if(stepBool2) {
+    (function animateSvg2() {
+      var basicTimeline = anime.timeline();
+      var basicTimeline2 = anime.timeline();
+      var basicTimeline3 = anime.timeline();
+      basicTimeline
+      .add({
+        targets: ['#svg-right-1', '#svg-right-2','#svg-right-3'],
+        opacity: 1,       
+        scale: [         
+          {value: 1.1, elasticity: 100},
+          {value: 1, elasticity: 100}
+        ],
+        delay: function(el,i) {
+          return 1000 * (1 + i);
+        },
+        easing: 'easeInOutQuad',
+        offset: '-=600'
+      })
+      .add({
+        targets: ['#svg-right-4', '#svg-right-5','#svg-right-6'],
+        opacity: 1,       
+        scale: [         
+          {value: 1.1, elasticity: 100},
+          {value: 1, elasticity: 100}
+        ],
+        delay: function(el,i) {
+          return 1000 * (1 + i);
+        },
+        easing: 'easeInOutQuad',
+        offset: '-=600'
+      })
+     
+      basicTimeline2
+      .add({
+        targets: ['#p-right-1','#p-right-2','#p-right-3'],
+        opacity: 1,       
+        delay: function(el,i) {
+          return 1000 * (1 + i);
+        },
+        easing: 'easeInOutQuad',
+        offset: '-=600'
+      })
+      .add({
+        targets: ['#p-right-4','#p-right-5','#p-right-6'],
+        opacity: 1,       
+        delay: function(el,i) {
+          return 1000 * (1 + i);
+        },
+        easing: 'easeInOutQuad',
+        offset: '-=600'
+      })
+
+      basicTimeline3
+      .add({
+        targets: ['#vg-right-1','#vg-right-2'],
+        opacity: 1,
+        translateX: '0',
+        width: {
+          value: '200'
+        },
+        easing: 'easeInOutQuad',
+        delay: function(el,i,l) {
+          return (1300 * (1 + i));
+        },
+        offset: '-=600'
+      })
+      .add({
+        targets: ['#vg-right-3','#vg-right-4','#vg-right-5'],
+        opacity: 1,
+        translateX: '0',
+        width: {
+          value: '200'
+        },
+        easing: 'easeInOutQuad',
+        delay: function(el,i,l) {
+          return (1100 * (1 + i));
+        },
+        offset: '-=600'
+      })
+
+      var stepRH2 = anime({
+        targets: '.steps-item__right-2__animation h2',
+        opacity: 1,       
+        delay: 200,
+        easing: 'easeInOutQuad',
+      });
+
+    })(); stepBool2 = false; }
+      
+    });
+
+    
+    
+
+
+  })();
+  
 })
